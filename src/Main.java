c003fe3c2928ad8f3d14df3f626128e83745c0f6
TorokNiki <niki.torok@gmail.com>
2020-01-29 10:25:59 +0100

    Java órai munka

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import felulet.SakkFelulet;
+import logika.SakkTabla;
+
+public class Main {
+    public static void main(String[] args) {
+        SakkFelulet s= new SakkFelulet();
+        SakkTabla t = new SakkTabla();
+        System.out.println(t);
+        t.lep(6,4,4,4);
+        System.out.println(t);
+        t.lep(6,3,4,2);//rossz lépés
+        System.out.println(t);
+        t.lep(1,4,3,4);
+        System.out.println(t);
+    }
+}
+
---
+++ src/felulet/SakkFelulet.java
@@ -0,0 +1,62 @@
+package felulet;
+
+import logika.SakkTabla;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SakkFelulet extends JFrame {
+    private Container foAblak;
+    private JPanel pnlJatekTabla;
+    private SakkTabla tabla;
+    public SakkFelulet() {
+        this.tabla= new SakkTabla();
+        this.initComponents();
+        this.sakkTablaMegjelenit();
+    }
+
+    private void initComponents() {
+    this.setTitle("Sakk 1.0");
+//    this.setUndecorated(true);
+    this.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+    int width= (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+    int hight= (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+    this.setSize(width,hight);
+    this.setAlwaysOnTop(true);
+
+    this.setLocationRelativeTo(null);
+    this.foAblak= this.getContentPane();
+    this.foAblak.setLayout(new BorderLayout(10,10));
+    this.pnlJatekTabla= new JPanel();
+    this.pnlJatekTabla.setLayout(new GridLayout(9,9));
+    this.foAblak.add(BorderLayout.CENTER,this.pnlJatekTabla);
+    this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    this.setVisible(true);
+    }
+    private void sakkTablaMegjelenit(){
+        this.pnlJatekTabla.removeAll();
+        this.pnlJatekTabla.add(new JLabel());
+        for (int i = 0; i <8 ; i++) {
+            JLabel hivatkozasABC= new JLabel();
+            hivatkozasABC.setText(String.format("%s.",(char)(i+65)));
+            hivatkozasABC.setHorizontalAlignment((SwingConstants.CENTER));
+            hivatkozasABC.setVerticalAlignment((SwingConstants.CENTER));
+            this.pnlJatekTabla.add(hivatkozasABC);
+        }
+
+        for (int i = 0; i <8 ; i++) {
+            JLabel hivatkozas123= new JLabel();
+            hivatkozas123.setText(String.format("%d.",(8-i)));
+            hivatkozas123.setHorizontalAlignment((SwingConstants.CENTER));
+            hivatkozas123.setVerticalAlignment((SwingConstants.CENTER));
+            this.pnlJatekTabla.add(hivatkozas123);
+            for (int j = 0; j <8 ; j++) {
+                Mezo m= new Mezo(i,j,tabla.getErtek(i,j));
+                this.pnlJatekTabla.add(m);
+            }
+        }
+        this.revalidate();
+        this.repaint();
+
+    }
+}
